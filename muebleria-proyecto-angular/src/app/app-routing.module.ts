86bcfab9eac74905b35aebf1beea0b755855c2c9
Sergio <sfonseca@estudiantec.cr>
2020-08-02 14:26:13 -0600

    initial commit, navbar and routing

--- muebleria-proyecto-angular/src/app/app-routing.module.ts
+++ muebleria-proyecto-angular/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { NationalManageComponent } from './views/national-manage/national-manage.component';
+import { GeneralManageComponent } from './views/general-manage/general-manage.component';
+import { ClientComponent } from './views/client/client.component';
+import { CeoComponent } from './views/ceo/ceo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'nationalManage',
+    component : NationalManageComponent
+  },
+  {
+    path: 'generalManage',
+    component : GeneralManageComponent
+  },
+  {
+    path: 'client',
+    component : ClientComponent
+  },
+  {
+    path: 'ceo',
+    component : CeoComponent
+  },
+  {
+    path: '**',
+    component : HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- muebleria-proyecto-angular/src/app/app.module.ts
+++ muebleria-proyecto-angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { ClientComponent } from './views/client/client.component';
+import { GeneralManageComponent } from './views/general-manage/general-manage.component';
+import { NationalManageComponent } from './views/national-manage/national-manage.component';
+import { CeoComponent } from './views/ceo/ceo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ClientComponent,
+    GeneralManageComponent,
+    NationalManageComponent,
+    CeoComponent
   ],
   imports: [
     BrowserModule,
