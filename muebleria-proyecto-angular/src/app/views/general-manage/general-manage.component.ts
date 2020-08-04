71ba3cebf0041915dd0053c4e68f345b8085cc30
Sergio <sfonseca@estudiantec.cr>
2020-08-04 10:49:41 -0600

    lista sucursales, empleados y salarios

--- muebleria-proyecto-angular/src/app/views/general-manage/general-manage.component.ts
+++ muebleria-proyecto-angular/src/app/views/general-manage/general-manage.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralManageComponent implements OnInit {
 
+  sucursalPais = false;
+  empleados = false;
+  salarios = false;
+
+  listaSucursalesPais = [{"name" : "Sucursal Cartago"},
+                         {"name" : "Sucursal Heredia"},
+                         {"name" : "Sucursal San José"},
+                         {"name" : "Sucursal Alajuela"}];
+  listaEmpleados =      [{"name" : "Kendall Tencio","salary": 3000},
+                         {"name" : "Sergio Fonseca","salary": 4000},
+                         {"name" : "Adrián Mora","salary": 5000},
+                         {"name" : "Sebastian Segura","salary": 6000}];
+
+
+
   constructor() { }
 
   ngOnInit() {
