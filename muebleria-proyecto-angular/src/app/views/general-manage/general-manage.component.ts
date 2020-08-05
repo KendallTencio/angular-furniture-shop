b7b8dc09db39510c82b92f0f954eb0f0bd51d3cf
Kendall Tencio Esquivel <kendalltencio132e@gmail.com>
2020-08-04 19:05:05 -0600

    Collapse básicos de Caso 11 y 13.

--- muebleria-proyecto-angular/src/app/views/general-manage/general-manage.component.ts
+++ muebleria-proyecto-angular/src/app/views/general-manage/general-manage.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralManageComponent implements OnInit {
 
-  constructor() { }
+  muebles = [
+    {
+      title: 'Silla',
+      id: '01',
+      description: 'Ejemplo de texto hablando del mueble.',
+      muebleProgress: '25'
+    },
+    {
+      title: 'Mesa',
+      id: '02',
+      description: 'Ejemplo de texto hablando del mueble.',
+      muebleProgress: '45'
+    }
+  ];
+
+  productos = [
+    {
+      title: 'Silla grande',
+      id: '01',
+      material: 'X y Y.',
+      price: '$60',
+      imagen: 'https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/11/furniture-logo.jpg'
+    },
+    {
+      title: 'Juego de sala',
+      id: '02',
+      material: 'X y Y.',
+      price: '$370',
+      imagen: 'https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/11/furniture-logo.jpg'
+    },
+    {
+      title: 'Mesa victoriana',
+      id: '03',
+      material: 'X y Y.',
+      price: '$100',
+      imagen: 'https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/11/furniture-logo.jpg'
+    }
+  ];
+
+  constructor() { 
+    
+
+  }
 
   ngOnInit() {
+
   }
 
 }
